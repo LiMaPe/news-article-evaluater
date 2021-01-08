e63a544aebc0f1bfcea23c082fdf3314bc3fc06b
LiMaPe <65661847+LiMaPe@users.noreply.github.com>
2021-01-08 09:34:19 +0100

    post route initial setup

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,30 @@
-function handleSubmit(event) {
+async function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    const body2 = {
+        url: "hej"
+    }
+    console.log("::: Form Submitted :::");
+
+    fetch('/test2', {
+        method: "POST",
+        body: JSON.stringify(body2)
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        console.log(res)
+        document.getElementById('results').innerHTML = res
     })
+
 }
 
 export { handleSubmit }
+
+
+
+
+
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,36 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+//Require and config dotenv. Config assigns the content of .env file to process.env.
+require("dotenv").config();
 
+//Define the env variables. process.env has the keys and values as defined in the .env file.
+const port = process.env.PORT
+const apiKey =  process.env.API_KEY;
+
+//Require express and set up an instance of app
+const fetch = require('node-fetch');
+const express = require('express');
 const app = express()
 
-//server looks for asset files in dist
+//Middleware
+//Require and set the body-parse 
+const bodyParser = require('body-parser')
+app.use(bodyParser.text());
+
+//Cors for cross origin allowance 
+const cors = require('cors');
+
+//Enables all CORS requests
+app.use(cors());
+
+//server looks for asset files in dist. Connect the server-side and client-side (browser) code.
 app.use(express.static('dist'))
+
+//Setup express server
+app.listen(port, () =>
+  console.log(`NEWS-app listening on port ${port}`),
+);
+
+var path = require('path')
+const mockAPIResponse = require('./mockAPI.js')
 
 console.log(__dirname)
 
@@ -13,11 +38,30 @@ app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function () {
-    console.log('This app is listening on port 8081!')
+app.post('/test2', function (req, res) {
+    console.log("req:", req.body)
+
+    fetch('https://api.mocki.io/v1/b043df5a')
+    .then(response => response.json())
+    .then(data => {
+        res.send(data)
+    });
+    
+
 })
 
-app.get('/test', function (req, res) {
+//Add a GET route that returns respons from mockAPI
+app.post('/test', function (req, res) {
+    console.log({req});
     res.send(mockAPIResponse)
 })
+
+
+
+//make api call to meaning cloud
+
+
+
+
+
+
