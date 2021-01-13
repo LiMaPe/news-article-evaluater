0a82f6704a3600dce9ca5614950294ce710743cc
LiMaPe <65661847+LiMaPe@users.noreply.github.com>
2021-01-13 15:32:45 +0100

    style

--- src/client/index.js
+++ src/client/index.js
@@ -6,6 +6,9 @@ import './styles/base.scss'
 import './styles/footer.scss'
 import './styles/form.scss'
 import './styles/header.scss'
+import './styles/cards.scss'
+import './styles/results.scss'
+import './styles/intro.scss'
 
 export {
     checkUrl,
